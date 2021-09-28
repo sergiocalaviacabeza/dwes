8b746f1e9e8ef0e1e4cf2bdfc5d70ed8d786ebff
sergiocalaviacabeza <sergicalabia@yahoo.es>
2021-09-28 16:20:39 +0100

    clase 28/09

--- UD2/ejercicios/ejercicio11.php
+++ UD2/ejercicios/ejercicio11.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Ejercicio 10 Equipo Basket</title>
+        <title>Ejercicio 11 Paises</title>
     </head>
         <body>
             <?php 
