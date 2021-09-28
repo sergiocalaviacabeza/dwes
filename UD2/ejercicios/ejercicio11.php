ffdb462e22825045a5c7bf5ad9164042975cdc6f
sergiocalaviacabeza <sergicalabia@yahoo.es>
2021-09-28 16:22:50 +0100

    clase2809

--- UD2/ejercicios/ejercicio11.php
+++ UD2/ejercicios/ejercicio11.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Ejercicio 11 Paises</title>
+        <title>Ejercicio 11 Pais</title>
     </head>
         <body>
             <?php 
