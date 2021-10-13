c8c963fd4d10a763ab61505842ffed416ae80180
sergiocalaviacabeza <sergicalabia@yahoo.es>
2021-10-13 21:08:48 +0200

    1310

--- UD3/ejercicios/form_ej6_test.php
+++ UD3/ejercicios/form_ej6_test.php
@@ -10,35 +10,30 @@
 <body>
     <h2>Test DAW 2</h2>
     <form method="post" action="">
-        <label for="resp1">1- Lenguaje de Servidor</label>
-        <select name="resp1[]" id="resp1" multiple>
-            <option value="c">C</option>
-            <option value="phyton">Phyton</option>
-            <option value="php">PHP</option>
-        </select>
-        <br><br>
-        <input type="submit" value="Enviar">
+        1- Lenguaje de Servidor: <br>
+        <input type="radio" name="resp1" value="c" > C <br>
+        <input type="radio" name="resp1" value="python" checked> Python<br>
+        <input type="radio" name="resp1" value="php" checked> PHP <br>
+        <br>
 
-        <label for="resp2">2- Programa Editor Código</label>
-        <select name="resp2[]" id="resp2" multiple>
-            <option value="visual">Visual Studio</option>
-            <option value="office">Libre Office</option>
-            <option value="paint">Paint</option>
-        </select>
-        <br><br>
+        2- Programa de editar código: <br>
+        <input type="radio" name="resp2" value="visual">Visual Studio <br>
+        <input type="radio" name="resp2" value="office">Libre Office<br>
+        <input type="radio" name="resp2" value="paint">Paint<br>
+        <br>
         <input type="submit" value="Enviar">
+        <br>
     </form>
-    
+
+        <br>
     <?php
-        $acierto=0;
-        if(isset($_POST) && !empty($_POST)){
-            if(resp1==php){
-                $acierto++;
-            }
-        }
-        else {
-            echo "No hemos recibido nada!";
-        }
+        $aciertos=0;
+
+        /*if (resp1==php){
+            aciertos ++;
+        }else if (resp2==visual){
+            aciertos ++;
+        }*/
         
         echo " Resultado = $aciertos respuestas correctas";
         
