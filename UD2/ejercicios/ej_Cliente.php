7f9428bd88471c1cff287692e1bb788d659d34a4
sergiocalaviacabeza <sergicalabia@yahoo.es>
2021-10-14 15:57:13 +0100

    1410

---
+++ UD2/ejercicios/ej_Cliente.php
@@ -0,0 +1,24 @@
+<?php
+
+class Cliente extends Persona {
+    private $saldo = 0;
+
+    function __construct ($nombre, $apellido, $edad, $saldo) {
+        parent::__construct ($nombre, $apellido, $edad);
+        $this->saldo = $saldo;
+    }
+
+    public function getSaldo() {
+        return $this->saldo;
+    }
+
+    public function setSaldo($saldo) {
+        return $this->saldo = $saldo;
+    }
+
+    public function __toString() {
+        return "Cliente: ".$this->getNombre();
+    }
+
+    
+}
---
+++ UD2/ejercicios/ejercicio3.php
@@ -0,0 +1,9 @@
+<?php
+$euro=166.386;
+$peseta=2000;
+$conversor=$peseta/$euro;
+
+echo "<br> 1 Euro = $euro <br>";
+echo "<br> Pesetas = $peseta <br>";
+echo "<br> Conversión euros - pesetas = $conversor euros <br>";
+
