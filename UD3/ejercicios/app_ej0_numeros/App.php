93813a852449a969ece9b3197b82aad1ac521830
sergiocalaviacabeza <sergicalabia@yahoo.es>
2021-10-18 17:15:15 +0100

    1810

---
+++ UD3/ejercicios/app_ej0_numeros/App.php
@@ -0,0 +1,63 @@
+<?php
+
+class App
+{
+    $numeros;
+
+    public function run(){
+        echo "Función matemática aleatoria";
+        $opcion = rand(0,2);
+        if ($opcion == 0){
+            echo "Cálculo números primos"
+            $this->primos();
+        } else if ($opcion == 1){
+            echo "Serie de Fibonacci"
+            $this->fibonacci(); 
+        } else {
+            echo "Potencia de 2"
+            $this->potencia2(); 
+        }
+
+    }
+    
+    public function primos(){
+        echo "Números primos entre 2 y 100: <br>";
+
+          for ($n = 2; $n <= 100; $n++) {
+            $esPrimo = true;
+            for ($i = 2; $i < $n; $i++) {
+              if ($n % $i == 0) {
+                $esPrimo = false;
+              }
+            }
+            if ($esPrimo) {
+              echo "$n&nbsp;&nbsp;&nbsp;";
+            }
+          }
+    }
+
+    public function fibonacci(){
+        $numero_anterior = 1;
+        $numero_posterior = 0;
+        $serie = 0;
+        $fin = 100;
+        echo "Serie de Fibonacci: ";
+        while ($serie < $fin){
+            echo $serie . ", ";
+            $serie = $numero_anterior + $numero_posterior;
+            $numero_posterior=$serie;
+        }
+    }
+
+    public function potencia2($numero,$potencia){
+        $potencia = 2;
+        $resultado = $numero;
+        for ($i=2;$i>1; $i--){
+            $resultado = $resultado * $numero;
+        }
+        $numero = $resultado;
+        return $numero;
+    }
+
+
+}
--- UD3/ejercicios/form_ej6_test.php
+++ UD3/ejercicios/form_ej6_test.php
@@ -10,33 +10,48 @@
 <body>
     <h2>Test DAW 2</h2>
     <form method="post" action="">
-        1- Lenguaje de Servidor: <br>
-        <input type="radio" name="resp1" value="c" > C <br>
-        <input type="radio" name="resp1" value="python" checked> Python<br>
-        <input type="radio" name="resp1" value="php" checked> PHP <br>
-        <br>
-
-        2- Programa de editar código: <br>
-        <input type="radio" name="resp2" value="visual">Visual Studio <br>
-        <input type="radio" name="resp2" value="office">Libre Office<br>
-        <input type="radio" name="resp2" value="paint">Paint<br>
-        <br>
+        <label for="resp1">1- Lenguaje de Servidor</label>
+        <select name="resp1[]" id="resp1" multiple>
+            <option value="c">C</option>
+            <option value="phyton">Phyton</option>
+            <option value="php">PHP</option>
+        </select>
+        <br><br>
         <input type="submit" value="Enviar">
-        <br>
+
+        <label for="resp2">2- Programa Editor Código</label>
+        <select name="resp2[]" id="resp2" multiple>
+            <option value="visual">Visual Studio</option>
+            <option value="office">Libre Office</option>
+            <option value="paint">Paint</option>
+        </select>
+        <br><br>
+        <input type="submit" value="Enviar">
     </form>
-
-        <br>
+    
     <?php
-        $aciertos=0;
+    if (isset($_POST['resp1']) && !empty($_POST)['resp1'])){
+        echo "Error";
+    } else if (isset($_POST['resp2']) && !empty($_POST['resp2'])){
+      echo "Error en el número 2 por el tipo de formato, no es numérico o está vacío";
+    }
 
-        if ($POST['resp1']=='php'){
-            $aciertos = $aciertos + 1;
-        }else if ($POST['resp1']=='visual'){
-            $aciertos = $aciertos + 1;
+      $resp1 = $_POST['resp1'];
+      $resp2 = $_POST['resp2'];
+
+        $aciertos=0;
+        if (resp1 == php){
+            $aciertos= $aciertos + 1;
+        }else if (resp2 == visual){
+            $aciertos= $aciertos + 1;
         }
-        
-        echo " Resultado = $aciertos respuestas correctas";
-        
+    }
+    else {
+        echo "No tienes ningún acierto";
+    }
+    
+      echo " Resultado = $aciertos respuestas correctas";
+    
   ?>
 </body>
 
