50ca61351e0aa90f42dadc19e024b81e83652e3c
sergiocalaviacabeza <sergicalabia@yahoo.es>
2021-11-16 16:21:53 +0000

    1611 - 17.20h

--- UD4/mipeluqueria/app/models/Employee.php
+++ UD4/mipeluqueria/app/models/Employee.php
@@ -14,7 +14,18 @@ require_once 'core/Model.php';
 class User extends Model
 {
     public static function all(){ 
-        //TODO        
+         //obtener conexión
+         $db = User::db();
+         //preparar consulta
+         $sql = "SELECT * FROM users";
+         //ejecutar
+         $statement = $db->query($sql); // query para ejecutar la consulta
+         //el resultado puede ser tomado usan las funciones de de PDO
+         //fetch recoge registro a registro. Si hay muchos requiere un bucle
+         //fetch_all recoge arrays
+         $users = $statement->fetchAll(PDO::FETCH_CLASS, User::class);
+         //retornar
+         return $users;       
     }
     public static function find($id){ 
         //TODO        
--- UD4/mipeluqueria/config/db.php
+++ UD4/mipeluqueria/config/db.php
@@ -2,6 +2,6 @@
 #config/db.php
 namespace Config;
 
-const DSN = 'mysql:dbname=mvc;host=db';
+const DSN = 'mysql:dbname=pelu;host=db';
 const USER = 'root';
 const PASSWORD = 'password';
