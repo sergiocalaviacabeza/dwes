1423cb937c758aeb2d8eb66bfde7ae989b473c9a
sergiocalaviacabeza <sergicalabia@yahoo.es>
2021-11-17 17:52:51 +0000

    1711 18.51h

---
+++ UD4/mipeluqueria/sql/servicios.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: db
+-- Tiempo de generación: 17-11-2021 a las 17:28:10
+-- Versión del servidor: 5.7.36
+-- Versión de PHP: 7.4.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pelu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicios`
+--
+
+CREATE TABLE `servicios` (
+  `ID` int(11) NOT NULL,
+  `SERVICIO` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `DESCRIPCIÓN` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `TIEMPO` int(11) NOT NULL,
+  `PRECIO` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `trabajador`
+--
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`SERVICIO`),
+  ADD UNIQUE KEY `ID` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+INSERT INTO `servicios` (`ID`, `SERVICIO`, `DESCRIPCION`, `TIEMPO`, `PRECIO`) VALUES
+(1, 'DETOX', 'Descongestiona y equilibra cabello', '50', '60'),
+(2, 'RESTAURADOR', 'Recupera y fortalece el cabello', '30', '50'),
+(3, 'INFOACTIVO', 'Actúa en la raíz', '30', '70'),
+(4, 'PROTEÍNAS', 'Rellena de materia el cabello', '50', '100'),
+(5, 'DETOX ORO', 'Descongestiona e hidrata cuero cabelludo', '30', '50')
+;
