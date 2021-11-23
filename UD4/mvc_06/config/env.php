24e943bf6daa790292ba92abac06fb5165b2f65d
sergiocalaviacabeza <sergicalabia@yahoo.es>
2021-11-23 14:59:45 +0000

    2311 15.59h

---
+++ UD4/mvc_06/config/env.php
@@ -0,0 +1,2 @@
+<?php
+    define ('PATH', '/mvc_06');
---
+++ UD4/mvc_06/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c3cb2f1aa08207381ffb562f01fef932b7295430',
+        'name' => 'alumno/mvc_06',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alumno/mvc_06' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c3cb2f1aa08207381ffb562f01fef932b7295430',
+            'dev_requirement' => false,
+        ),
+        'dompdf/dompdf' => array(
+            'pretty_version' => 'v0.6.2',
+            'version' => '0.6.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../dompdf/dompdf',
+            'aliases' => array(),
+            'reference' => 'cc06008f75262510ee135b8cbb14e333a309f651',
+            'dev_requirement' => false,
+        ),
+        'phenx/php-font-lib' => array(
+            'pretty_version' => '0.2.2',
+            'version' => '0.2.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phenx/php-font-lib',
+            'aliases' => array(),
+            'reference' => 'c30c7fc00a6b0d863e9bb4c5d5dd015298b2dc82',
+            'dev_requirement' => false,
+        ),
+    ),
+);
