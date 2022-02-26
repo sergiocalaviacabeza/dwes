ba8106ab0ccaba1e7bcf01bc7d3552a591111209
sergiocalaviacabeza <sergicalabia@yahoo.es>
2022-02-26 19:30:32 +0100

    19.30h 2602

--- UD4/equipo/config/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-#config/db.php
-namespace Config;
-
-const DSN = 'mysql:dbname=mvc;host=db;charset=utf8';
-const USER = 'root';
-const PASSWORD = 'password';
--- UD4/equipo/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-
-//echo "Inicio<br>";
-session_start();
-$app = new \Core\App();
