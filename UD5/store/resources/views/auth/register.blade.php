315818950932f09afadf5cd14455b678283cf64f
sergiocalaviacabeza <sergicalabia@yahoo.es>
2022-06-01 18:38:04 +0100

    0106 19.37h

--- UD5/store/resources/views/auth/register.blade.php
+++ UD5/store/resources/views/auth/register.blade.php
@@ -26,6 +26,20 @@
                         </div>
 
                         <div class="row mb-3">
+                            <label for="address" class="col-md-4 col-form-label text-md-end">{{ __('Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address" autofocus>
+
+                                @error('address')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
--- UD5/store/resources/views/home.blade.php
+++ UD5/store/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Registrado') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,10 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Bienvenido a RecordStore!') }}
+                    <br>
+                    <br>
+                    <a href="records" class="btn btn-primary float-right">Ver Catálogo de Discos</a>
                 </div>
             </div>
         </div>
